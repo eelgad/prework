e27f2f210d518c2ff24db309582c46d2beee7656
eelgad <156122428+eelgad@users.noreply.github.com>
2025-08-19 22:38:43 -0400

    added prework files to repository

---
+++ IntroduceStudentTests/IntroduceStudentTests.swift
@@ -0,0 +1,17 @@
+//
+//  IntroduceStudentTests.swift
+//  IntroduceStudentTests
+//
+//  Created by Elisheva El-Gad on 8/18/25.
+//
+
+import Testing
+@testable import IntroduceStudent
+
+struct IntroduceStudentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
